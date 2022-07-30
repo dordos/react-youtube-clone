1858c566c96d9b39f9c4877a5e92d07e47b3dc10
dordos <pdhssf@gmail.com>
2022-07-30 16:34:07 -0400

    create menu-bar

---
+++ src/layout/HeaderBar/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faMagnifyingGlass,
+  faMicrophone,
+  faEllipsisVertical,
+} from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+import "./style.scss";
+
+const HeaderBar = () => {
+  const logoIcon = require("../../img/logo.png");
+  return (
+    <div className="header__container">
+      <div className="menu">
+        <div className="faBars">
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <div className="faYoutube">
+          <img src={logoIcon} alt="" />
+          <span>YuTube</span>
+        </div>
+      </div>
+      <div className="search">
+        <input type="text" />
+        <div className="faMagnifyingGlass">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </div>
+        <div className="faMicrophone">
+          <FontAwesomeIcon icon={faMicrophone} />
+        </div>
+      </div>
+      <div className="login">
+        <div className="faEllipsisVertical">
+          <FontAwesomeIcon icon={faEllipsisVertical} />
+        </div>
+        <div>
+          <FontAwesomeIcon icon={faUserCircle} />
+          <span>SIGN IN</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderBar;
